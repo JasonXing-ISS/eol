d771e1c939f8565717a9874b5befe67ee12f109b
Jason Xing <84940638+JasonXing-ISS@users.noreply.github.com>
2023-08-01 15:54:35 -0400

    second commit for lf

--- tmp/test.java
+++ tmp/test.java
@@ -1,5 +1,10 @@
-public Partly {
+class Partly {
     public static void main (String[] args) {
         String TEST = "LF as stage 0";
+        String Second = "edit";
     }
+}
+
+Public class test{
+    
 }
